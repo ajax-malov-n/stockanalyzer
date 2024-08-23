19a1a285a47fcf4a2c2ea52b6cfc52fedb2096ce
Nikita Malov <malov.n@ajax.systems>
2024-08-23 15:07:30 +0300

    resolve issues

--- src/main/kotlin/systems/ajax/malov/stockanalyzer/config/beanpostprocessor/LogExecutionTimeAnnotationBeanPostProcessor.kt
+++ src/main/kotlin/systems/ajax/malov/stockanalyzer/config/beanpostprocessor/LogExecutionTimeAnnotationBeanPostProcessor.kt
@@ -1,6 +1,7 @@
 package systems.ajax.malov.stockanalyzer.config.beanpostprocessor
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
@@ -8,29 +9,33 @@ import org.springframework.stereotype.Component
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
 
+
 @Component
 @Order(Ordered.LOWEST_PRECEDENCE)
 class LogExecutionTimeAnnotationBeanPostProcessor : BeanPostProcessor {
-    private val beanMap = HashMap<String, Any>()
+    @Value("\${logExecutionTime.enabled}")
+    private var logExecutionTimeEnabled: Boolean = false
+
+    private val beanMap = HashMap<String, Pair<Class<*>, List<Method>>>()
 
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
-        if (bean.javaClass.methods.any { it.isAnnotationPresent(LogExecutionTime::class.java) }) {
-            beanMap[beanName] = bean
+        val methods = bean.javaClass.methods.filter { it.isAnnotationPresent(LogExecutionTime::class.java) }
+        if (methods.isNotEmpty()) {
+            beanMap[beanName] = bean.javaClass to methods
         }
         return bean
     }
 
     override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
-        return beanMap[beanName]?.let { beanClass ->
+        return beanMap[beanName]?.let { (beanClass, methods) ->
             Proxy.newProxyInstance(
-                beanClass.javaClass.getClassLoader(),
-                getAllInterfaces(beanClass.javaClass).toTypedArray()
-            )
-            { _, method, args ->
-                if (isMethodAnnotatedInSuperclass(method, beanClass)) {
+                beanClass.getClassLoader(), getAllInterfaces(beanClass).toTypedArray()
+            ) { _, method, args ->
+                if (logExecutionTimeEnabled && methods.any { areMethodsEqual(it, method) }) {
                     val before = System.nanoTime()
                     val result = method.invoke(bean, *(args ?: emptyArray()))
-                    log.info("Execution time of ${method.name} is ${(System.nanoTime() - before) / 1_000_000} ms")
+                    val executionTimeInMs = (System.nanoTime() - before) / 1_000_000
+                    log.info("Execution time of {} is {} ms", method.name, executionTimeInMs)
                     result
                 } else {
                     method.invoke(bean, *(args ?: emptyArray()))
@@ -40,26 +45,18 @@ class LogExecutionTimeAnnotationBeanPostProcessor : BeanPostProcessor {
     }
 
     private fun getAllInterfaces(clazz: Class<*>): Set<Class<*>> {
-        val interfaces = mutableSetOf<Class<*>>()
-        var currentClass: Class<*>? = clazz
-        while (currentClass != null) {
-            interfaces.addAll(currentClass.interfaces)
-            currentClass = currentClass.superclass
-        }
-        return interfaces
-    }
-
-    private fun isMethodAnnotatedInSuperclass(method: Method, bean: Any): Boolean {
-        val clazz: Class<*> = bean.javaClass
-        try {
-            val superMethod = clazz.getDeclaredMethod(method.name, *(method.parameterTypes ?: emptyArray()))
-            return superMethod.isAnnotationPresent(LogExecutionTime::class.java)
-        } catch (_: NoSuchMethodException) {
-            return false
-        }
+        return generateSequence(clazz) { it.superclass }
+            .flatMap { it.interfaces.asSequence() }
+            .toSet()
     }
 
     companion object {
         private val log = LoggerFactory.getLogger(LogExecutionTimeAnnotationBeanPostProcessor::class.java)
+    }
+
+    fun areMethodsEqual(method1: Method, method2: Method): Boolean {
+        return method1.name == method2.name &&
+                method1.returnType == method2.returnType &&
+                method1.parameterTypes.contentEquals(method2.parameterTypes)
     }
 }
