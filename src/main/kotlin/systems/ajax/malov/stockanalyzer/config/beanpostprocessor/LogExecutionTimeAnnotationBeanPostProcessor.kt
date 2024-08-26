fc9a577093ef0b9b9f267743a2eac595bec91e2a
Nikita Malov <malov.n@ajax.systems>
2024-08-26 12:03:17 +0300

    refactor bpp

--- src/main/kotlin/systems/ajax/malov/stockanalyzer/config/beanpostprocessor/LogExecutionTimeAnnotationBeanPostProcessor.kt
+++ src/main/kotlin/systems/ajax/malov/stockanalyzer/config/beanpostprocessor/LogExecutionTimeAnnotationBeanPostProcessor.kt
@@ -1,25 +1,23 @@
 package systems.ajax.malov.stockanalyzer.config.beanpostprocessor
 
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanPostProcessor
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
 
 
+@ConditionalOnProperty(name = ["logExecutionTime.enabled"], havingValue = "true")
 @Component
-@Order(Ordered.LOWEST_PRECEDENCE)
 class LogExecutionTimeAnnotationBeanPostProcessor : BeanPostProcessor {
-    @Value("\${logExecutionTime.enabled}")
-    private var logExecutionTimeEnabled: Boolean = false
 
     private val beanMap = HashMap<String, Pair<Class<*>, List<Method>>>()
 
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
-        val methods = bean.javaClass.methods.filter { it.isAnnotationPresent(LogExecutionTime::class.java) }
+        val methods: List<Method> = bean.javaClass.methods.filter {
+            it.isAnnotationPresent(LogExecutionTime::class.java)
+        }
         if (methods.isNotEmpty()) {
             beanMap[beanName] = bean.javaClass to methods
         }
@@ -29,34 +27,56 @@ class LogExecutionTimeAnnotationBeanPostProcessor : BeanPostProcessor {
     override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
         return beanMap[beanName]?.let { (beanClass, methods) ->
             Proxy.newProxyInstance(
-                beanClass.getClassLoader(), getAllInterfaces(beanClass).toTypedArray()
-            ) { _, method, args ->
-                if (logExecutionTimeEnabled && methods.any { areMethodsEqual(it, method) }) {
-                    val before = System.nanoTime()
-                    val result = method.invoke(bean, *(args ?: emptyArray()))
-                    val executionTimeInMs = (System.nanoTime() - before) / 1_000_000
-                    log.info("Execution time of {} is {} ms", method.name, executionTimeInMs)
-                    result
+                beanClass.getClassLoader(),
+                getAllInterfaces(beanClass)
+            ) { _, proxiedMethod, args ->
+                if (isRequiredProxy(methods, proxiedMethod)) {
+                    logMethodExecution(proxiedMethod, bean, args)
                 } else {
-                    method.invoke(bean, *(args ?: emptyArray()))
+                    proxiedMethod.invoke(bean, *(args ?: emptyArray()))
                 }
             }
         } ?: bean
     }
 
-    private fun getAllInterfaces(clazz: Class<*>): Set<Class<*>> {
+    private fun isRequiredProxy(
+        methods: List<Method>,
+        proxiedMethod: Method,
+    ) = methods.any { originalMethod ->
+        areMethodsEqual(
+            originalMethod,
+            proxiedMethod
+        )
+    }
+
+    private fun logMethodExecution(
+        proxiedMethod: Method,
+        bean: Any,
+        args: Array<out Any>?,
+    ): Any? {
+        val before = System.nanoTime()
+        return try {
+            proxiedMethod.invoke(bean, *(args ?: emptyArray<Any?>()))
+        } finally {
+            val executionTimeInMs = (System.nanoTime() - before) / 1_000_000
+            log.info("Execution time of {} is {} ms", proxiedMethod.name, executionTimeInMs)
+        }
+    }
+
+    private fun getAllInterfaces(clazz: Class<*>): Array<Class<*>> {
         return generateSequence(clazz) { it.superclass }
             .flatMap { it.interfaces.asSequence() }
             .toSet()
+            .toTypedArray()
+    }
+
+    private fun areMethodsEqual(originalMethod: Method, proxiedMethod: Method): Boolean {
+        return originalMethod.name == proxiedMethod.name &&
+                originalMethod.returnType == proxiedMethod.returnType &&
+                originalMethod.parameterTypes.contentEquals(proxiedMethod.parameterTypes)
     }
 
     companion object {
         private val log = LoggerFactory.getLogger(LogExecutionTimeAnnotationBeanPostProcessor::class.java)
-    }
-
-    fun areMethodsEqual(method1: Method, method2: Method): Boolean {
-        return method1.name == method2.name &&
-                method1.returnType == method2.returnType &&
-                method1.parameterTypes.contentEquals(method2.parameterTypes)
     }
 }
