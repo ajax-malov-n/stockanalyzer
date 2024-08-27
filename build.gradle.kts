d134cd98a2f8d831fc074def9f8d5264bfe08028
Nikita Malov <malov.n@ajax.systems>
2024-08-27 17:19:38 +0300

    improve gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.spring") version "1.9.23"
-    id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
-    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.dependency.management)
+    alias(libs.plugins.detekt)
     `java-test-fixtures`
 }
 
@@ -23,9 +23,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
-    implementation("io.finnhub:kotlin-client:2.0.20")
-//    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation(libs.mockito.kotlin)
+    implementation(libs.finnhub.kotlin.client)
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
