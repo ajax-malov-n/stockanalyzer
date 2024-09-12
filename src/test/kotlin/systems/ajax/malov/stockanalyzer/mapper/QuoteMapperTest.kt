044dcac18dfc1553afeb850fafeb359914f571c4
Nikita Malov <malov.n@ajax.systems>
2024-09-12 19:04:19 +0300

    test QuoteMapper

--- src/test/kotlin/systems/ajax/malov/stockanalyzer/mapper/QuoteMapperTest.kt
+++ src/test/kotlin/systems/ajax/malov/stockanalyzer/mapper/QuoteMapperTest.kt
@@ -1,5 +1,6 @@
 package systems.ajax.malov.stockanalyzer.mapper
 
+import java.math.BigDecimal
 import org.junit.jupiter.api.Test
 import stockanalyzer.utils.QuoteFixture.quote
 import stockanalyzer.utils.StockFixture.TEST_STOCK_SYMBOL
@@ -15,6 +16,15 @@ class QuoteMapperTest {
         val expected = unsavedStockRecord()
 
         val actual = quote().toStockRecord(TEST_STOCK_SYMBOL, testDate())
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    fun `should map quote with change and percentChange as null values to stock`() {
+        val expected = unsavedStockRecord().copy(change = BigDecimal.ZERO, percentChange = BigDecimal.ZERO)
+
+        val actual = quote().copy(d = null, dp = null).toStockRecord(TEST_STOCK_SYMBOL, testDate())
 
         assertEquals(expected, actual);
     }
